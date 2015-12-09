3245940109719310efcbf059dd6764888006137f
dbaddou <dbaddou@yahoo.com>
2015-12-09 11:53:00 -0500

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php 
+$title = 'Nous contacter'; 
+include 'header.php'; 
+?> 
+<h1>
+<?php 
+echo $title; 
+?>
+</h1>
+ <!-- Mettre ici le formulaire de contact --> 
+<?php 
+include 'footer.php'; 
+?>
