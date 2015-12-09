a87a078a73a266d1d6753e2248550bdeb64ed260
dbaddou <dbaddou@yahoo.com>
2015-12-09 11:54:17 -0500

    Create catalogue.php

---
+++ catalogue.php
@@ -0,0 +1,13 @@
+<?php 
+$title = 'Notre catalogue'; 
+include 'header.php'; 
+?> 
+<h1>
+<?php 
+echo $title; 
+?>
+</h1> <!-- Mettre ici le contenu de la page catalogue --> 
+<?php 
+include 'footer.php'; 
+?>
+
