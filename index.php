0dc54df5d57bafc2a529ecd71503714c6a677921
dbaddou <dbaddou@yahoo.com>
2015-12-09 11:49:14 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+$title = 'Bienvenue au inclusion test'; 
+include 'header.php'; 
+?>
+ <h1>
+<?php 
+echo $title; 
+?>
+</h1> 
+<!-- Mettre ici le contenu de la page d'accueil --> 
+<?php 
+include 'footer.php'; 
+?>
